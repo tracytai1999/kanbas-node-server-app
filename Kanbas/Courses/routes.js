9cb7b7b835f97cc3f626c2b97e7c2b4286752ca8
tracytai1999 <tracytai1999@gmail.com>
2024-11-17 23:08:32 -0800

    test

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -2,7 +2,7 @@ import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
 export default function CourseRoutes(app) {
-  app.post("/api/courses/:courseId/modules", (req, res) => {
+  app.post("api/courses/:courseId/modules", (req, res) => {
     const { courseId } = req.params;
     const module = {
       ...req.body,
@@ -11,7 +11,7 @@ export default function CourseRoutes(app) {
     const newModule = modulesDao.createModule(module);
     res.send(newModule);
   });
-  app.post("/api/courses/:courseId/assignments", (req, res) => {
+  app.post("api/courses/:courseId/assignments", (req, res) => {
       const { courseId } = req.params;
       const assignment = {
         ...req.body,
@@ -20,28 +20,28 @@ export default function CourseRoutes(app) {
       const newAssignment = assignmentsDao.createAssignment(assignment);
       res.send(newAssignment);
   });
-  app.get("/api/courses/:courseId/modules", (req, res) => {
+  app.get("api/courses/:courseId/modules", (req, res) => {
     const { courseId } = req.params;
     const modules = modulesDao.findModulesForCourse(courseId);
     res.json(modules);
   });
-  app.get("/api/courses/:courseId/assignments", (req, res) => {
+  app.get("api/courses/:courseId/assignments", (req, res) => {
     const { courseId } = req.params;
     const assignments = assignmentsDao.findAssignmentsForCourse(courseId);
     res.json(assignments);
   });
-  app.put("/api/courses/:courseId", (req, res) => {
+  app.put("api/courses/:courseId", (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
     dao.updateCourse(courseId, courseUpdates);
     res.sendStatus(204);
   });
-  app.delete("/api/courses/:courseId", (req, res) => {
+  app.delete("api/courses/:courseId", (req, res) => {
     const { courseId } = req.params;
     dao.deleteCourse(courseId);
     res.sendStatus(204);
   });
-  app.get("/api/courses", (req, res) => {
+  app.get("api/courses", (req, res) => {
     const courses = dao.findAllCourses();
     res.send(courses);
   });
