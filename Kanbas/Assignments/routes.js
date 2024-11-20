521c8345a6dd665f3ed51349cd6b3f88c291d1b5
tracytai1999 <tracytai1999@gmail.com>
2024-11-19 22:59:06 -0800

    final changes

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,12 +1,18 @@
 import * as assignmentsDao from "./dao.js";
 export default function AssignmentRoutes(app) {
-  app.put("/api/assignments/:assignmentId", (req, res) => {
+app.put("/api/assignments", (req, res) => {
     const { assignmentId } = req.params;
     const assignmentUpdates = req.body;
     assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
     res.sendStatus(204);
 });
- app.delete("/api/assignments/:assignmentId", (req, res) => {
+app.put("/api/assignments/:assignmentId", (req, res) => {
+  const { assignmentId } = req.params;
+  const assignmentUpdates = req.body;
+  assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
+  res.sendStatus(204);
+});
+ app.delete("/api/assignments", (req, res) => {
    const { assignmentId } = req.params;
    assignmentsDao.deleteAssignment(assignmentId);
    res.sendStatus(204);
