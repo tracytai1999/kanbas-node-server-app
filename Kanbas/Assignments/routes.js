c451b6adb8a2c9358a4a4f7929fe3b06309b46bc
tracytai1999 <tracytai1999@gmail.com>
2024-11-29 20:22:19 -0800

    a6

--- Kanbas/Assignments/routes.js
+++ Kanbas/Assignments/routes.js
@@ -1,6 +1,14 @@
 import * as assignmentsDao from "./dao.js";
 export default function AssignmentRoutes(app) {
 
+const updateAssignment = async (req, res) => {
+  const { assignmentId } = req.params;
+  const assignmentUpdates = req.body;
+  await assignmentsDao.updateAssignment(assignmentId, assignmentUpdates);
+  res.sendStatus(204);
+};
+app.put("/api/assignments/:assignmentId", updateAssignment);
+
 const deleteAssignment = async (req, res) => {
    const { assignmentId } = req.params;
    await assignmentsDao.deleteAssignment(assignmentId);
